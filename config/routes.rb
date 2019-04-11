91e986d4b678a58760022cbc3f29e1d377e83058
akio yonamine <gaharatic@icloud.com>
2019-04-11 12:33:36 +0000

    Implement advanced login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   post '/signup',  to: 'users#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy' 
+  delete '/logout', to: 'sessions#destroy'
   resources :users
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -20,7 +20,8 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", user_path(@user)
     delete logout_path
     assert_not is_logged_in?
-    assert_redirected_to root_path
+    assert_redirected_to root_url
+    delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
@@ -39,6 +40,18 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_template 'users/show'
     assert is_logged_in?
+  end
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: "1")
+    assert_equal cookies["remember_token"], assigns(:user).remember_token
+  end
+
+  test "login without remembering" do
+    log_in_as(@user, remember_me: "1")
+    delete logout_path
+    log_in_as(@user, remember_me: "0")
+    assert_empty cookies["remember_token"]
   end
 
 end
