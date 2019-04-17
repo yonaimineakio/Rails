6f9c858d189fcd46a59360511d469cae001056ad
yonaimineakio <gaharatic@icloud.com>
2019-04-17 08:34:18 +0000

    Change Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,10 +95,10 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.raise_delivery_errors=true
-  config.action_mailer.delivery_method= :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
   host = "secure-sea-30759.herokuapp.com"
-  config.action_mailer.default_url_options={host:host}
+  config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
